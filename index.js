0df5dab02596b38d960b0d9fee5be34545160322
root <root@ubuntu-docker.myguest.virtualbox.org>
2025-06-13 12:25:27 +0500

    Intentionally broke know corect index.js for testing pipeline error

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const http = require('http');
-const port = 3000;
 
-
-const requestHandler = (req, res) => {
-  res.end('Hello from CI/CD pipeline! testing');
-
-const server = http.createServer(requestHandler);
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+const server = http.createServer((req, res) => {
+  res.end('Hello World!');
 });
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
